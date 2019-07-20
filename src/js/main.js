074dd205cfa0c1fbae8ac709358224b9fa200728
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-21 00:02:21 +0200

    feat: src files

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,47 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
+// const name = "Ola"
+// const city = "Poznaniu";
+// console.log(`Mam na imię ${name} i mieszkam w ${city}`);
+// const about = document.querySelector ('.about__paragraph--js');
+// console.log(about);
+// console.log(about.innerHTML);
+
+// about.innerHTML = 'Pozdrawiam !!!';
+
+// let yourAge, yourName;
+
+// function hello (yourName, yourAge) {
+//     console.log(`Witaj ${yourName} ! Masz ${yourAge} lat`);
+
+// };
+
+// hello("Karolina", "30");
+
+// const welcome = (yourName, yourAge) => {
+//     console.log(`Witaj ${yourName}! Masz ${yourAge} lat!`);
+// }
+
+// welcome('Asia',15);
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+console.log(navigationSwitcher);
+
+const navigationList = document.querySelector('.navigation__list--js');
+console.log(navigationList);
+
+navigationSwitcher.addEventListener('click', (e) => {
+    navigationList.classList.toggle('navigation__list--visible');
+    if (navigationList.classList.contains('navigation__list--visible')) {
+        navigationSwitcher.innerHTML = '&#935;';
+    } else {
+        navigationSwitcher.innerHTML = '&#926;';
+    }
+
+});
+
+
 console.log(`Hello world!`);
 
 
