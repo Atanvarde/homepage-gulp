79e03cb5286591a5591aabdd4838bd767aefa6ff
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-24 08:51:37 +0200

    feat: local storage

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -42,10 +42,10 @@ if ('serviceWorker' in navigator) {
 // welcome('Asia',15);
 
 const navigationSwitcher = document.querySelector('.navigation__switcher--js');
-console.log(navigationSwitcher);
+// console.log(navigationSwitcher);
 
 const navigationList = document.querySelector('.navigation__list--js');
-console.log(navigationList);
+// console.log(navigationList);
 
 navigationSwitcher.addEventListener('click', (e) => {
     navigationList.classList.toggle('navigation__list--visible');
@@ -58,6 +58,80 @@ navigationSwitcher.addEventListener('click', (e) => {
 });
 
 
-console.log(`Hello world!`);
+// console.log(`Hello world!`);
 
+// for ( let i = 0; i < 100; i++) {
+//   console.log(`${i+1} powtórzenie`);
+// }
 
+// let a = 1;
+// console.log(a++);
+// console.log(a);
+
+// let b = 2;
+// console.log(b--);
+// console.log(b);
+
+// let c = 1;
+// console.log(++c);
+// console.log(c);
+
+// let d = 1;
+// console.log(--d);
+// console.log(d);
+
+// const faces = ['😏', '😎', '😠', 'text'];
+
+// for (let i = 0; i < faces.length; i++) {
+//   console.log(`i: ${i}`);
+//   console.log(`element ${i + 1}: ${faces[i]}`);
+// }
+
+// for (let face of faces) {
+//   console.log(face);
+// }
+
+// let a = 10;
+
+// while (a > 0) {
+//   console.log(a--);
+// }
+
+// let n = 3;
+// while (n < 9) {
+//   console.log(n);
+//   n = n + 3;
+// }
+// console.log(n);
+
+// let b = 4;
+
+// do {
+// b = b+3;
+// console.log(b);
+// } while (b <9) ;
+
+const person = {
+  name: 'ola',
+  surname: 'gardo'
+}
+
+// for (let property in person) {
+//   console.log(`${property}: ${person[property]}`);
+// }
+
+console.log(person);
+
+const jsonPerson = JSON.stringify(person);
+
+console.log(jsonPerson);
+
+localStorage.setItem('person', jsonPerson);
+
+const personStringify = localStorage.getItem('person');
+
+const newPerson = JSON.parse(personStringify);
+
+console.log(newPerson);
+
+// localStorage.removeItem('person');
