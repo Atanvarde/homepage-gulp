8d73d42f590212ebb85d4a89928fc66c448cc3a1
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-24 22:56:48 +0200

    fix: webpack configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const outputDir = path.resolve(__dirname, 'dist/js/');
 module.exports = {
     devtool: 'eval-source-map',
-    entry: path.resolve(__dirname, 'src/js/main.js'),
+    entry: {main: path.resolve(__dirname, 'src/js/main.js'),
+    localstorage: path.resolve(__dirname, 'src/js/localstorage.js')},
     mode: 'production',
     output: {
         path: outputDir,
-        filename: 'main.js'
+        filename: '[name].js'
     }
 };
