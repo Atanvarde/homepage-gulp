723e87833018098cd5f9d8696c4030ff94491627
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-24 11:25:09 +0200

    fix: localstorage

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -3,31 +3,32 @@ const person = {
     surname: 'gardo'
   }
 
-console.log(person);
+  console.log(person);
 
-const jsonPerson = JSON.stringify(person);
-
-console.log(jsonPerson);
-
-localStorage.setItem('person', jsonPerson);
-
-const personStringify = localStorage.getItem('person');
-
-const newPerson = JSON.parse(personStringify);
-
-console.log(newPerson);
-
-// localStorage.removeItem('person');
-
-const focusInput = document.querySelector('.focus--js');
-
-if (localStorage.getItem('focusInput')) {
-  focusInput.value = localStorage.getItem('focusInput');
-};
-
-
-focusInput.addEventListener('keyup', (e) => {
-  localStorage.setItem('focusInput', e.target.value);
-});
-
-// localStorage.removeItem('focusInput');
+  const jsonPerson = JSON.stringify(person);
+  
+  console.log(jsonPerson);
+  
+  localStorage.setItem('person', jsonPerson);
+  
+  const personStringify = localStorage.getItem('person');
+  
+  const newPerson = JSON.parse(personStringify);
+  
+  console.log(newPerson);
+  
+  // localStorage.removeItem('person');
+  // localStorage.removeItem('name','Ola');
+  
+  const focusInput = document.querySelector('.focus--js');
+  
+  if (localStorage.getItem('focusInput')) {
+    focusInput.value = localStorage.getItem('focusInput');
+  };
+  
+  
+  focusInput.addEventListener('keyup', (e) => {
+    localStorage.setItem('focusInput', e.target.value);
+  });
+  
+//   localStorage.removeItem('focusInput');
